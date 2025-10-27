4ef15eb3764e8cbaeaeec1392f5ba38f0379320b
j0n0347 <jonathanphillips80@gmail.com>
2025-10-27 17:14:36 -0400

    current known build of fiance scraper

--- python/src/scripts/classes/nvda.py
+++ python/src/scripts/classes/nvda.py
@@ -1,6 +1,8 @@
 from classes.base import BaseDriver
 from selenium.webdriver.remote.webelement import WebElement
 
+# todo 
+# add scraping for nvidia stock
 class NvdaDriver(BaseDriver):
     def print():
         print("booty")
